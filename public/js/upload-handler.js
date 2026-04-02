9e8a84fbe052bb6b5cf9bca5a3605a92f905c09b
Anthony DeDominic <adedomin@gmail.com>
2026-04-02 18:07:23 -0400

    rewrite frontend.

--- public/js/upload-handler.js
+++ public/js/upload-handler.js
@@ -16,33 +16,15 @@
 
 'use strict';
 
-const banner = document.getElementById('banner-color');
-const statusMsg = document.getElementById('status-mesg');
+const statusMsg = document.getElementById('status-msg');
 const files = document.getElementById('file');
 const dropzone = document.getElementById('dropzone');
 const submit = document.getElementById('submit');
-
-const xhrExt = Symbol("xhr-ext");
+const uploads = document.getElementById('uploads');
+const tmpl = document.getElementById('upload-tmpl');
 
 function setInfo(message) {
     statusMsg.textContent = message;
-    banner.classList.remove('is-success');
-    banner.classList.remove('is-danger');
-    banner.classList.add('is-primary');
-}
-
-function setFailBanner(message) {
-    statusMsg.textContent = message;
-    banner.classList.remove('is-primary');
-    banner.classList.remove('is-success');
-    banner.classList.add('is-danger');
-}
-
-function setSuccessBanner(message) {
-    statusMsg.textContent = message;
-    banner.classList.remove('is-danger');
-    banner.classList.remove('is-primary');
-    banner.classList.add('is-success');
 }
 
 function createImageFigure(file) {
@@ -63,123 +45,7 @@ function createImageFigure(file) {
             }
         });
     }
-    
-    const figureEl = document.createElement('figure');
-    figureEl.classList.add('image');
-    figureEl.classList.add('is-128x128');
-    figureEl.style.overflow = 'hidden';
-    figureEl.appendChild(imgEl);
-
-    const box = document.createElement('div');
-    box.classList.add('media-left');
-    box.appendChild(figureEl);
-    
-    return box;
-}
-
-function createFileBox(file) {
-    const url = document.createElement('a');
-    url.href = '';
-    url.textContent = `Uploading ${file.name}...`;
-
-    const copyUrlButton = document.createElement('button');
-    copyUrlButton.classList.add('button');
-    copyUrlButton.textContent = 'Copy to clipboard';
-    copyUrlButton.dataset.wasClicked = false;
-    copyUrlButton.addEventListener('click', function(ev) {
-        ev.preventDefault();
-
-        // unset any existing clicked button
-        document.querySelectorAll('button[data-was-clicked="true"]')
-            .forEach(button => {
-                button.textContent = 'Copy to clipboard';
-                button.dataset.wasClicked = false;
-                button.classList.remove('is-success');
-                button.classList.remove('is-danger');
-            });
-        // indicate this button was clicked
-        ev.target.dataset.wasClicked = true;
-
-        let fakeInput = document.createElement('textarea');
-        fakeInput.value = url.href;
-
-        document.body.appendChild(fakeInput);
-        fakeInput.select();
-
-        if (document.execCommand('copy')) {
-            ev.target.classList.add('is-success');
-            ev.target.textContent = 'Copied';
-        }
-        else {
-            ev.target.classList.add('is-danger');
-            ev.target.textContent = 'Failed to copy to clipboard';
-        }
-
-        document.body.removeChild(fakeInput);
-    });
-
-    const contentBox = document.createElement('p');
-    contentBox.classList.add('has-text-centered');
-    contentBox.appendChild(url);
-    contentBox.appendChild(document.createElement('br'));
-    contentBox.appendChild(copyUrlButton);
-
-    const contentContentBox = document.createElement('div');
-    contentContentBox.classList.add('content');
-    contentContentBox.appendChild(contentBox);
-
-    const contentContainer = document.createElement('div');
-    contentContainer.classList.add('media-content');
-    contentContainer.appendChild(contentContentBox);
-
-    const mediaContainer = document.createElement('div');
-    mediaContainer.classList.add('media');
-    mediaContainer.appendChild(createImageFigure(file));
-    mediaContainer.appendChild(contentContainer);
-
-    const box = document.createElement('div');
-    box.classList.add('box');
-    box.classList.add('column');
-    box.classList.add('is-6');
-    box.style.padding='5px';
-    box.appendChild(mediaContainer);
-
-    return { box, url };
-}
-
-function finishedUpload(ev) {
-    dropzone.textContent = 'Select or Drop Files';
-
-    const xhr = ev.target;
-    const { box, url }  = xhr[xhrExt];
-
-    let res;
-
-    try {
-        res = JSON.parse(xhr.responseText);
-    }
-    catch (e) {
-        res = { status: "error", msg: null };
-        if (xhr.status === 413) {
-            res.msg = "Your image is too large!"; 
-        }
-        else if (xhr.status === 0) {
-            res.msg = "Unknown error. Your browser did not process the response.";
-        }
-        else {
-            res.msg = `Unknown error. Did not receive a url for uploaded image. HTTP Code: ${xhr.status}`;
-        }
-    }
-
-    if (xhr.status != 200 || res.status == 'error') {
-        if (box && box.parentNode) box.parentNode.removeChild(box); 
-        return setFailBanner(res.msg || 'unknown error');
-    }
-
-    url.href = res.msg;
-    url.textContent = res.msg;
-    setSuccessBanner('Successfully Uploaded');
-    document.getElementById('uploads').appendChild(box);
+    return imgEl;
 }
 
 let movingDotPos = -1;
@@ -196,20 +62,107 @@ function incrementProgress(el) {
     }
 }
 
-function handleFile(file) {
-    if (file.type.indexOf('image') != 0 &&
-        file.type.indexOf('video') != 0 ) {
+function handleRes(code, txt) {
+    let res;
+    try {
+        // in case the server returns html (reverse proxy)
+        res = JSON.parse(txt);
+    }
+    catch (e) {
+        res = { status: "error", msg: null };
+        if (code === 413) {
+            res.msg = "Your image is too large!"; 
+        }
+        else if (code === 0) {
+            res.msg = "Unknown error. Your browser did not process the response.";
+        }
+        else {
+            res.msg = `Unknown error. Did not receive a url for uploaded image. HTTP Code: ${code}`;
+        }
+    }
+    return res;
+}
 
-        dropzone.textContent = 'Select or Drop Files';
-        return setFailBanner('You can only upload images or videos');
+function createFailBox(file, msg) {
+    const box = createUploadBox(file, msg);
+    const up = box.querySelector('.upload-g');
+    up.classList.add('failed');
+    const btn =  up.querySelector('button');
+    btn.disabled = true;
+    btn.textContent = 'Failed';
+    up.querySelector('a').replaceWith(msg);
+    console.log(box);
+    return box;
+}
+
+function createUploadBox(file, link) {
+    const box = tmpl.content.cloneNode(true);
+
+    const media = box.querySelector('.img');
+    media.appendChild(createImageFigure(file));
+
+    const url = box.querySelector('a');
+    url.href = link;
+    url.textContent = link;
+
+    const copyBtn = box.querySelector('button');
+    copyBtn.addEventListener('click', ev => {
+        ev.preventDefault();
+        // unset any existing clicked button
+        document.querySelectorAll('button[data-was-clicked="true"]')
+            .forEach(button => {
+                button.textContent = 'Copy link';
+                button.dataset.wasClicked = false;
+                button.classList.remove('selected');
+                button.classList.remove('failed');
+            });
+        // indicate this button was clicked
+        ev.target.dataset.wasClicked = true;
+
+        let fakeInput = document.createElement('textarea');
+        fakeInput.value = url.href;
+        document.body.appendChild(fakeInput);
+        fakeInput.select();
+        if (document.execCommand('copy')) {
+            ev.target.classList.add('selected');
+            ev.target.textContent = 'Copied';
+        }
+        else {
+            ev.target.classList.add('failed');
+            ev.target.textContent = 'Failed to copy';
+        }
+        document.body.removeChild(fakeInput);
+    });
+
+    return box;
+}
+
+function handleFile(file) {
+    if (file.type.indexOf('image') != 0 && file.type.indexOf('video') != 0 ) {
+        setInfo('You can only upload images or videos');
+        return;
     }
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', './upload');
-    xhr.upload.addEventListener('progress', incrementProgress);
-    xhr.addEventListener('loadend', finishedUpload);
-    xhr[xhrExt] = createFileBox(file);
-    xhr.send(file);
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', './upload');
+        xhr.upload.addEventListener('progress', incrementProgress);
+        xhr.addEventListener('loadend', () => {
+            const code = xhr.status
+            const res = handleRes(code, xhr.responseText);
+            let box;
+            if (code != 200 || res.status == 'error') {
+                box = createFailBox(file, res.msg);
+                reject(res.msg);
+            }
+            else {
+                box = createUploadBox(file, res.msg);
+                resolve();
+            }
+            uploads.appendChild(box);
+        });
+        xhr.send(file);
+    });
 }
 
 function changeFileLabel(el) {
@@ -218,17 +171,29 @@ function changeFileLabel(el) {
         submit.disabled = false;
 }
 
-function uploadFile(el) {
-    setInfo('Uploading...');
-    Array.prototype.forEach.call(el.files, handleFile);
+function handleAllUploads(files) {
     submit.disabled = true;
+    setInfo('Uploading...');
+    Promise.allSettled(Array.prototype.map.call(files, handleFile)).then(results => {
+        dropzone.textContent = 'Select or Drop Files';
+        setInfo('Uploaded all');
+        results.forEach(r => {
+            if (r.status === 'rejected') setInfo('An Upload failed.');
+        });
+    });
+}
+
+function uploadFile(el) {
+    handleAllUploads(el.files);
 }
 
 function dropHandle(el) {
     el.preventDefault();
-    const data = el.dataTransfer;
-    if (data.files) Array.prototype.forEach.call(data.files, handleFile);
-    submit.disabled = true;
+    const files = el?.dataTransfer?.files;
+    if (files != null) {
+        submit.disabled = true;
+        handleAllUploads(files);
+    }
 }
 
 function dragover(el) {
